63791f50869ed73ef2c7f001a7a7e6a161e04769
kanata0510 <kawakana0510@gmail.com>
2023-12-10 21:55:11 +0900

    infodataget complete

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -3,3 +3,33 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script>
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+export default {
+  async mounted() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDb6Y-8ischpWY57SxMxk3TYD76EDtA9ZY",
+      authDomain: "hinnyari-album.firebaseapp.com",
+      databaseURL: "https://hinnyari-album.firebaseio.com",
+      projectId: "hinnyari-album",
+      storageBucket: "hinnyari-album.appspot.com",
+      messagingSenderId: "11865309438",
+      appId: "1:11865309438:web:6825490f2e93401975e5af",
+      measurementId: "G-ETT6D58FR6"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+    const querySnapshot = await getDocs(collection(db, "hinnyaris"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+}
+</script>
