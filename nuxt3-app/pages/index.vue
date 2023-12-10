e442c19d7946fec05cc6562a7b06ce1d9e70e352
kanata0510 <kawakana0510@gmail.com>
2023-12-10 23:28:17 +0900

    signinwithgoogle complete

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -2,11 +2,43 @@
     <div>
         ひんやりあるばむ
     </div>
+    <div class="links">
+        <button @click="signin" class="button--green">SignIn</button>
+    </div>
 </template>
 
+
+
 <script>
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+
 export default {
+    auth: undefined,
     name: "Home",
+    mounted(){
+        const firebaseConfig = {
+                apiKey: "AIzaSyDb6Y-8ischpWY57SxMxk3TYD76EDtA9ZY",
+                authDomain: "hinnyari-album.firebaseapp.com",
+                databaseURL: "https://hinnyari-album.firebaseio.com",
+                projectId: "hinnyari-album",
+                storageBucket: "hinnyari-album.appspot.com",
+                messagingSenderId: "11865309438",
+                appId: "1:11865309438:web:6825490f2e93401975e5af",
+                measurementId: "G-ETT6D58FR6"
+            };
+
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
+            // Initialize Firebase Authentication and get a reference to the service
+            this.auth = getAuth(app);
+    },
+    methods: {
+        signin: function () {
+            const provider = new GoogleAuthProvider();
+            signInWithRedirect(this.auth, provider);
+        }
+    }
 }
 </script>
 
