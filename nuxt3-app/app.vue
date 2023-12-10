8506dee9feb985ed473df2b74e38ac84c2da6684
kanata0510 <kawakana0510@gmail.com>
2023-12-10 20:47:09 +0900

    infodataset complete

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -3,3 +3,37 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script>
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+export default {
+  mounted() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDb6Y-8ischpWY57SxMxk3TYD76EDtA9ZY",
+      authDomain: "hinnyari-album.firebaseapp.com",
+      databaseURL: "https://hinnyari-album.firebaseio.com",
+      projectId: "hinnyari-album",
+      storageBucket: "hinnyari-album.appspot.com",
+      messagingSenderId: "11865309438",
+      appId: "1:11865309438:web:6825490f2e93401975e5af",
+      measurementId: "G-ETT6D58FR6"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+
+    // Add a new document in collection "cities"
+    addDoc(collection(db, "hinnyaris"), {
+      evaluationValue: 1,
+      imageUrl: "test",
+      mapUrl: "test",
+      objectName: "test",
+      spotName: "test"
+    });
+  }
+}
+</script>
