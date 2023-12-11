8db9127078a7e49e0f1f168eb420c72e02112b40
kanata0510 <kawakana0510@gmail.com>
2023-12-11 23:42:40 +0900

    infodatadelete complete

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -3,3 +3,36 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script>
+import { initializeApp } from "firebase/app";
+import { getFirestore, deleteDoc, doc } from "firebase/firestore";
+
+export default {
+  db: undefined,
+  mounted() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDb6Y-8ischpWY57SxMxk3TYD76EDtA9ZY",
+      authDomain: "hinnyari-album.firebaseapp.com",
+      databaseURL: "https://hinnyari-album.firebaseio.com",
+      projectId: "hinnyari-album",
+      storageBucket: "hinnyari-album.appspot.com",
+      messagingSenderId: "11865309438",
+      appId: "1:11865309438:web:6825490f2e93401975e5af",
+      measurementId: "G-ETT6D58FR6"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    // Initialize Cloud Firestore and get a reference to the service
+    this.db = getFirestore(app);
+    this.delete();
+  },
+  methods: {
+    delete: async function () {
+      const docname = "2QKnhbHen7RPO6hEwUzI";
+      await deleteDoc(doc(this.db, "hinnyaris", docname));
+    }
+  }
+}
+</script>
