83314ba6a0eeaf524a3debec27267cebdea1e1ea
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-13 01:40:27 +0900

    評価値コンポーネント作成/ブランチ作っただけ

---
+++ nuxt3-app/components/atoms/Evaluation/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        
+    </div>
+ </template>
+ 
+ <script>
+ export default {
+     name: 'Evaluation', // コンポーネントのフォルダ名と同じ Buttonなど
+     props: {
+
+     }
+ }
+ </script>
+ 
+ 
+ <style lang="scss" scoped>
+ @import './index.scss';
+ </style>
