d0152183971c6e257a57c67bfad412d5b77e162c
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-13 13:24:00 +0900

    評価値表示/完成

--- nuxt3-app/components/atoms/Evaluation/index.vue
+++ nuxt3-app/components/atoms/Evaluation/index.vue
@@ -1,19 +1,41 @@
 <template>
-    <div>
-        
+    <div class="Evaluation">
+        <div>
+            <span v-for="n in evalIntValue" :key="n">★</span>
+            <span v-for="n in (5 - evalIntValue)" :key="n">☆</span>
+        </div>
+        <div class="evalVal">（<span class="evalVal">{{ evalValue }}</span>）</div>
     </div>
- </template>
- 
- <script>
- export default {
-     name: 'Evaluation', // コンポーネントのフォルダ名と同じ Buttonなど
-     props: {
+</template>
 
-     }
- }
- </script>
- 
- 
- <style lang="scss" scoped>
- @import './index.scss';
- </style>
+<script>
+export default {
+    name: 'Evaluation',
+    props: {
+        evaluationSumValue: {
+            type: Number,
+            required: true,
+        },
+        evaluationCount: {
+            type: Number,
+            required: true,
+        }
+    },
+    data: () => {
+        return {
+            evalValue: 0,
+            evalIntValue: 0,
+        }
+    },
+    mounted() {
+        const val = this.evaluationSumValue / this.evaluationCount;
+        this.evalValue = Math.round(val * 100) / 100;
+        this.evalIntValue = Math.floor(val);
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
