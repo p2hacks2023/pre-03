c7c0e9d73dd5847eb8c3ab46f32aaa30e7411430
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-14 04:44:10 +0900

    HinnyariDeleteBox/完成

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <HinnyariDeleteBox style="margin: 20px auto;" name="hogehoge" evaluation-sum-value=10 evaluation-count=3 imgPath="images/testImage.jpg" />
     </div>
 </template>
 
