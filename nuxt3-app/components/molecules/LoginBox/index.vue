d5b795eae43eb4a5b6f95500d3d41610d6004abf
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-13 23:04:37 +0900

    LoginBox作る/branci作っただけ

---
+++ nuxt3-app/components/molecules/LoginBox/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        ひんやりあるばむ
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginBox",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
+
