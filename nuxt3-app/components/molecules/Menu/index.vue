7e69d2c189e0658853ad066ee21ce1e426ed46d8
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-14 05:09:22 +0900

    Menu/完成

---
+++ nuxt3-app/components/molecules/Menu/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Menu">
+        <router-link to="/">
+            <font-awesome-icon :icon="['fas', 'house']" class="icon home" />
+        </router-link>
+        <router-link to="/MyPostList">
+            <div class="circle icon">
+                <font-awesome-icon :icon="['fas', 'user']" class="user" />
+            </div>
+        </router-link>
+        <router-link to="/Game">
+            <font-awesome-icon :icon="['fas', 'gamepad']" class="icon game" />
+        </router-link>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "Menu"
+}
+</script>
+  
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        ひんやりあるばむ
+        <Menu />
     </div>
 </template>
 
