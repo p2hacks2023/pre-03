3fdeb7989ae2ff03dc4295a6a1e7218226666202
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-14 13:48:51 +0900

    windowサイズ取得

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -2,23 +2,4 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-<<<<<<< HEAD
 </template>
-=======
-</template>
-
-<script>
-function initMap() {
-    // マップの初期化
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: { lat: 36.38992, lng: 139.06065 }
-    });
-
-    // クリックイベントを追加
-    map.addListener('click', function (e) {
-        this.getClickLatLng(e.latLng, map);
-    });
-}
-</script>
->>>>>>> feat/GoogleMapComponent
