45447706547330d6d431333f1ebcb4461bfbe309
b1022054 <b1022054@fun.ac.jp>
2023-12-14 13:28:36 +0900

    技術検証を終了

--- nuxt3-app/pages/FontAwesomeTest/index.vue
+++ nuxt3-app/pages/FontAwesomeTest/index.vue
@@ -2,8 +2,8 @@
     <div class="fai">
         <!-- https://fontawesome.com/search -->
         <!-- free版ではBrands IconとClassic Iconが利用可能。-->
-
-        <font-awesome-icon :icon="['fas', 'house']" style="height: 100px"/>
+        
+        
         <font-awesome-icon :icon="['fas', 'magnifying-glass']" style="width: 10%"/>
         <font-awesome-icon :icon="['fas', 'user']" />
         <font-awesome-icon :icon="['fas', 'circle-plus']" style="{color: 'red'}"/>
@@ -15,17 +15,29 @@
         <font-awesome-icon :icon="['fas', 'arrow-up']" />
         <font-awesome-icon :icon="['fas', 'paperclip']" />
         <font-awesome-icon :icon="['fas', 'arrow-up-from-bracket']" />
-        <font-awesome-icon :icon="['far', 'circle-user']" />
         <font-awesome-icon :icon="['fas', 'gear']" style="height: 100px"/>
         <font-awesome-icon :icon="['fas', 'trash']" />
         <font-awesome-icon :icon="['fas', 'bookmark']" />
         <font-awesome-icon :icon="['far', 'bookmark']" />
         <font-awesome-icon :icon="['far', 'folder-open']" />
         <font-awesome-icon :icon="['fas', 'right-to-bracket']" />
-        <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" />
-
+        <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']"/>
         <font-awesome-icon :icon="['far', 'user']" style="{color: 'red'}"/>
     </div>
+    <div class="star">
+            <font-awesome-icon :icon="['fas', 'minus']" />
+            <font-awesome-icon :icon="['fas', 'star']" size="2x"/>
+            <font-awesome-icon :icon="['fas', 'star']" size="2x"/>
+            <font-awesome-icon :icon="['fas', 'star']" size="2x"/>
+            <font-awesome-icon :icon="['far', 'star-half-stroke']" size="2x"/>
+            <font-awesome-icon :icon="['far', 'star']" size="2x"/>
+            <font-awesome-icon :icon="['fas', 'plus']" />
+        </div>
+        <div class="buttom">
+            <font-awesome-icon :icon="['fas', 'house']"/>
+            <font-awesome-icon :icon="['fas', 'circle-user']"/>
+            <font-awesome-icon :icon="['fas', 'gamepad']"/>
+        </div>
 </template>
 
 <script>
