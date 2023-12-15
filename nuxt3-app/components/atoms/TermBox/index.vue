82875ae3f02e9e78307865607669a1f94024856c
ida <discoverywant30@gmail.com>
2023-12-15 22:58:01 +0900

    プロパティの子プロパティを読み込めない

---
+++ nuxt3-app/components/atoms/TermBox/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box">
+    <div class="margin">
+      <div v-for="term in terms">
+        <div class="heading">
+          {{ term.heading }}
+        </div>
+        <div v-for="text in term.text">
+          {{ text.main1 }}
+          {{ text.indent1 }}
+        </div>
+        <br/><br/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import term from '../assets/term.json';
+export default {
+  name: "TermBox",
+  data: () => ({
+    terms: term,
+    texts: term.text
+  })
+}
+</script>
+
+<style lang="scss" scoped>@import './index.scss';
+</style>
--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         ひんやりあるばむ
-        <OpinionBox/>
+        <TermBox/>
     </div>
 </template>
 
