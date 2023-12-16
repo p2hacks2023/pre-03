4f10b6d40abb55351873bcd1fe49ae4d2eaac0ff
ida <discoverywant30@gmail.com>
2023-12-16 12:33:50 +0900

    いったんプッシュ

---
+++ nuxt3-app/components/atoms/TermHeading/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="heading">
+    {{ term.heading }}
+  </div>
+  <br/>
+</template>
+
+<script>
+import term from '../assets/term.json';
+export default {
+  name: "TermHeading",
+  data: () => ({
+    terms: term,
+  }),
+}
+</script>
+
+<style lang="scss" scoped>@import './index.scss';
+</style>
--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         ひんやりあるばむ
+        <TermHeading/>
     </div>
 </template>
 
